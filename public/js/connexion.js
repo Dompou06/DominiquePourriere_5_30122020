2f097b115c0a33204962b6346ba8e0c803c6c85b
Dompou06 <30805395+Dompou06@users.noreply.github.com>
2021-01-06 10:11:59 +0100

    Essai debogueur

--- public/js/connexion.js
+++ public/js/connexion.js
@@ -1,15 +1,15 @@
-//On crée une fonction qui recevra les paramètres de connection à la base de données
+//On crée une fonction qui recevra la méthode (GET ou POST) les paramètres de connection à la base de données
 //Et les paramètres lors de l'envoi de données (data et contentType) facultatifs via = null
 let request = function (method, url, data=null, contentType=null) {
     //On crée une promise qui se résoudra plus tard (asyncrone)
     return new Promise(function(resolve, reject){
         //On crée un objet permettant d'interagir avec une bd
         let request = new XMLHttpRequest();
-        //On écoute les changement d'état de l'objet
+        //On écoute les changements d'état de request
         request.onreadystatechange = function () {
-            //Si l'état arrive à étape 4 : (0)création puis (1)appel de open(), (2)appel de send(), (3)téléchargement via responseText, (4) Fin de l'opération 
+            //Si le dernier état est l'étape 4 : (0)création puis (1)appel de open(), (2)appel de send(), (3)téléchargement via responseText, (4) Fin de l'opération 
                 if (request.readyState === 4) {
-                        //On vérifie qu'il n'y pas d'erreur dans le statut de lapromise vers mongodb (200 pour Get et 201 pour Post)
+                        //On vérifie qu'il n'y pas d'erreur dans le statut de la promise vers mongodb (200 pour Get et 201 pour Post)
                         if(request.status === 200 || request.status === 201) {
                             resolve(request.responseText);
                         }
